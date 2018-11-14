87b647583117e044b749825afd99b11368a1dda7
Robert Schulte <schulte.robertw@outlook.com>
2018-11-14 10:05:40 -0600

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,13 +1,11 @@
 class ProductsController < ApplicationController
 
   def index
-    @cart = cart
   end
 
   def add
     # byebug
     cart << params[:product]
-    @cart = cart
     redirect_to products_path
   end
 
